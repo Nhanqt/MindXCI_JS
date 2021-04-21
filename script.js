0c11938db7e51a353dbc728a0adcd1f36ed4f03a
QuangTrongNhan <NhanQTSE140052@fpt.edu.vn>
2021-04-21 12:59:40 +0700

    21.4.2021

--- script.js
+++ script.js
@@ -58,31 +58,81 @@ const listItem4 = [
   },
 ];
 
-window.onload = function () {
-  for (let item of navItems) {
-    document.getElementsByClassName("menu")[0].innerHTML +=
-      `<span>` + item.name + `</span>`;
-  }
+// window.onload = function () {
+//   for (let item of navItems) {
+//     document.getElementsByClassName("menu")[0].innerHTML +=
+//       `<span>` + item.name + `</span>`;
+//   }
 
-  console.log(navItems[0]);
+//   console.log(navItems[0]);
 
-  for (let item of listItem1) {
-    document.getElementsByTagName("span")[0].innerHTML +=
-      `<ul> <li>` + item.name + `</li> </ul>`;
-  }
+//   for (let item of listItem1) {
+//     document.getElementsByTagName("span")[0].innerHTML +=
+//       `<ul> <li>` + item.name + `</li> </ul>`;
+//   }
 
-  for (let item of listItem2) {
-    document.getElementsByTagName("span")[1].innerHTML +=
-      `<ul> <li>` + item.name + `</li> </ul>`;
-  }
+//   for (let item of listItem2) {
+//     document.getElementsByTagName("span")[1].innerHTML +=
+//       `<ul> <li>` + item.name + `</li> </ul>`;
+//   }
 
-  for (let item of listItem3) {
-    document.getElementsByTagName("span")[2].innerHTML +=
-      `<ul> <li>` + item.name + `</li> </ul>`;
-  }
+//   for (let item of listItem3) {
+//     document.getElementsByTagName("span")[2].innerHTML +=
+//       `<ul> <li>` + item.name + `</li> </ul>`;
+//   }
 
-  for (let item of listItem4) {
-    document.getElementsByTagName("span")[3].innerHTML +=
-      `<ul> <li>` + item.name + `</li> </ul>`;
-  }
+//   for (let item of listItem4) {
+//     document.getElementsByTagName("span")[3].innerHTML +=
+//       `<ul> <li>` + item.name + `</li> </ul>`;
+//   }
+// };
+
+let renderItem = (list) => {
+  html = "";
+  console.log(list);
+  list.map((item) => {
+    html += `
+      <li>${item.name}</li>
+      `;
+  });
+  return html;
 };
+
+const fillItemByName = (name) => {
+  var html = "";
+  switch (name) {
+    case 1:
+      html = renderItem(listItem1);
+      break;
+    case 2:
+      html = renderItem(listItem2);
+      break;
+    case 3:
+      html = renderItem(listItem3);
+      break;
+    case 4:
+      html = renderItem(listItem4);
+      break;
+  }
+  return html;
+};
+
+const fillData = () => {
+  var html = "";
+  navItems.map((item) => {
+    html += `
+
+    <span>
+      ${item.name}
+      <ul>
+        ${fillItemByName(parseInt(item.name.charAt(item.name.length - 1), 10))}
+      </ul>
+    </span>
+
+    `;
+    return;
+  });
+  document.getElementById("menu").innerHTML = html;
+};
+
+fillData();
